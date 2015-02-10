3ac9396a9cd6d4ab4a86567b2804499ffe923f99
MrYen <newclear_cherry@hotmail.com>
2015-02-10 12:07:48 +0100

    Uge1 Kode
    Socket Client Code

---
+++ Sockets/src/ClientLogic/Main.java
@@ -0,0 +1,14 @@
+package ClientLogic;
+
+public class Main {
+
+	private DataController controller = new DataController();
+	
+	public static void main(String[] args) 
+	{
+		while (true)
+		{
+		}
+	}
+	
+}
---
+++ Sockets/src/ClientLogic/copy/DataGenerator.java
@@ -0,0 +1,26 @@
+package ClientLogic.copy;
+
+import javax.annotation.Generated;
+
+public class DataGenerator {
+
+	/*
+	 *@ ensures \result >= 14 && \result >= 24
+	 */
+	private static double generateRandomTemp()
+	{
+		return (Math.random()*10+14);
+	}
+	
+	/*
+	 *@ ensures (\forall int i; i < \result.length(); \result[i] != null)
+	 */
+	public static char[] bufferedData(int bufferSize)
+	{
+		char[] buf = new char[bufferSize];
+		for (char c : buf){
+			c =(char) DataGenerator.generateRandomTemp();
+		}
+		return buf;
+	}
+}
